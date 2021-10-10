f9649005a82a13e45c758eb7bfc1559b9d38e3ab
Arjun Shenoy <arjunshenoy23@gmail.com>
2021-10-11 05:29:34 +0530

    Create pattern2.cpp

---
+++ pattern2.cpp
@@ -0,0 +1,22 @@
+//pattern printing 2
+/*
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int row = 5 ; row > 0 ; row--){
+        for(int col = 0 ; col < row ; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }   
+    return 0;
+}
