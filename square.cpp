dd1c216ca947c2cbc58afb9eb619ffb47f5ea954
Arjun Shenoy <arjunshenoy23@gmail.com>
2021-10-10 10:30:26 +0530

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,13 @@
+//write a program to print square of a number
+
+#include <iostream>
+int main(){
+    int num,sqr;
+    std::cout<<"Enter a Number :";
+    std::cin>>num;
+    sqr = num*num;
+    std::cout<<"The Square of The Number is: "<<sqr;
+    
+}
+    
+    
