8050820b6aa022912519d0d2a4fe289fbcaf5a50
Arjun Shenoy <arjunshenoy23@gmail.com>
2021-10-10 19:40:47 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,22 @@
+//pattern printing 1
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int row = 0 ; row < 5 ; row++){
+        for(int col = 0 ; col <= row ; col++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }   
+    return 0;
+}
